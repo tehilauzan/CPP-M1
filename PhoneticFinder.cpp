8ae84a0bb77ebcebe8b7c9902d65384cee2ec9ee
tehilauzan <46137999+tehilauzan@users.noreply.github.com>
2020-07-23 09:50:05 +0300

    Add files via upload

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdexcept>
+#include "PhoneticFinder.hpp"
+
+
+string phonetic::find(string text, string word) {
+    return "happy";
+}
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using namespace std;
+namespace phonetic{
+    string find(string text, string word) ;
+    }
